bcb0ff5cd2446b8941ef4ebdd7474e4514a73922
Trond Hindenes <trond@hindenes.com>
2016-11-17 23:54:41 +0100

    code

---
+++ Backend/Controllers/ServerNameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    public class ServerNameController : ApiController
+    {
+        public Thing Get()
+        {
+            var thisThing = new Thing();
+            var hostHeader = Request.RequestUri.Host;
+            thisThing.localComputerName = System.Environment.GetEnvironmentVariable("computername");
+            thisThing.localHostHeader = hostHeader;
+            return thisThing;
+
+        }
+    }
+}
