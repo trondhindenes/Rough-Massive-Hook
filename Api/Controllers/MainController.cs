f7c7253bc7eae9a259cda2162c1a00d77839cad3
Trond Hindenes <trond@hindenes.com>
2016-11-17 23:53:37 +0100

    code

---
+++ Api/Controllers/MainController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Api.Models;
+using System.Configuration;
+
+namespace Api.Controllers
+{
+    public class MainController : ApiController
+    {
+        public ServiceInfo Get()
+        {
+            var client = new WebClient();
+            client.Headers.Add("content-type", "applicaton/json");
+            String url = ConfigurationManager.AppSettings["backend"];
+            string backendServerNameUrl = String.Format("{0}/api/ServerName", url);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var webResult = client.DownloadString(backendServerNameUrl);
+
+            Thing thisThing = Newtonsoft.Json.JsonConvert.DeserializeObject<Thing>(webResult);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+
+            ServiceInfo thisServiceInfo = new ServiceInfo();
+            thisServiceInfo.apiServer = System.Environment.GetEnvironmentVariable("computername");
+            thisServiceInfo.backendServer = thisThing.localComputerName;
+            thisServiceInfo.backendServerHostHeader = thisThing.localHostHeader;
+            thisServiceInfo.backendServerUrl = url;
+            thisServiceInfo.backendResponseTimeMs = elapsedMs.ToString();
+            return thisServiceInfo;
+        }
+    }
+}
---
+++ Api/Models/Thing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+    public class Thing
+    {
+        public String localComputerName { get; set; }
+        public string localHostHeader { get; set; }
+    }
+}
