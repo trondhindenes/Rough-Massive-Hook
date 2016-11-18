fd814a2157c0f0cdeae0d80cb093726cacb58cf5
Trond Hindenes <trond@hindenes.com>
2016-11-18 21:18:28 +0100

    moar fixin

--- Build/nuget-pack.ps1
+++ Build/nuget-pack.ps1
@@ -12,6 +12,7 @@ foreach ($file in $nuspecfiles)
     $fullname = $file.fullname
     $basename = $file.BaseName
     Write-Output "Found nuspec file at $fullname"
-    & nuget.exe pack $fullname -properties "owners=$env:APPVEYOR_REPO_COMMIT_AUTHOR;Authors=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION;id=$basename;description=$env:APPVEYOR_REPO_COMMIT_MESSAGE -verbose"
+    write-output "Running command nuget.exe pack $fullname -properties owners=$env:APPVEYOR_REPO_COMMIT_AUTHOR;Authors=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION;id=$basename;description=$env:APPVEYOR_REPO_COMMIT_MESSAGE -verbose"
+    & nuget.exe pack $fullname -properties "author=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION;id=$basename;title=$basename;description=$env:APPVEYOR_REPO_COMMIT_MESSAGE -verbose"
 
 }
