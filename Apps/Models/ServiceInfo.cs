1829d4594ad1d75324a949cca4d889613e986d80
Trond Hindenes <trond@hindenes.com>
2016-11-18 18:27:01 +0100

    new things

---
+++ Apps/Models/ServiceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class ServiceInfo
+    {
+        public String apiServer { get; set; }
+        public String backendServer { get; set; }
+        public String backendServerUrl { get; set; }
+        public String backendResponseTimeMs { get; set; }
+        public String backendServerHostHeader { get; set; }
+    }
+}
