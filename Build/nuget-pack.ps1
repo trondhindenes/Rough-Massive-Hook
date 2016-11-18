cdd3ae24a3a8f98b96d5b66825b47471574f4647
Trond Hindenes <trond@hindenes.com>
2016-11-18 21:12:48 +0100

    fiiiiixin

--- Build/nuget-pack.ps1
+++ Build/nuget-pack.ps1
@@ -9,8 +9,9 @@ $nuspecfiles = get-childitem *.nuspec -Recurse
 foreach ($file in $nuspecfiles)
 {
     $fullname = $file.fullname
+    $basename = $file.BaseName
     Write-Output "Found nuspec file at $fullname"
-    & nuget.exe pack $fullname -properties "owners=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION -verbose"
+    & nuget.exe pack $fullname -properties "owners=$env:APPVEYOR_REPO_COMMIT_AUTHOR;authors=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION;id=$basename;description=$env:APPVEYOR_REPO_COMMIT_MESSAGE -verbose"
 
 }
 
