64525570b5d1321ae54be9b6da2ea61f9905b793
Trond Hindenes <trond@hindenes.com>
2016-11-18 21:14:46 +0100

    moar fixin

--- Build/nuget-pack.ps1
+++ Build/nuget-pack.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 Write-output "Env variables:"
 ls env:
 
@@ -11,13 +12,6 @@ foreach ($file in $nuspecfiles)
     $fullname = $file.fullname
     $basename = $file.BaseName
     Write-Output "Found nuspec file at $fullname"
-    & nuget.exe pack $fullname -properties "owners=$env:APPVEYOR_REPO_COMMIT_AUTHOR;authors=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION;id=$basename;description=$env:APPVEYOR_REPO_COMMIT_MESSAGE -verbose"
+    & nuget.exe pack $fullname -properties "owners=$env:APPVEYOR_REPO_COMMIT_AUTHOR;Authors=$env:APPVEYOR_REPO_COMMIT_AUTHOR;version=$env:APPVEYOR_BUILD_VERSION;id=$basename;description=$env:APPVEYOR_REPO_COMMIT_MESSAGE -verbose"
 
 }
-
-
-$content = $content -replace '\$version\$',$versionStr
-
-$content | Out-File $root\nuget\MarkdownLog.compiled.nuspec
-
-& $root\NuGet\NuGet.exe pack $root\nuget\MarkdownLog.compiled.nuspec
