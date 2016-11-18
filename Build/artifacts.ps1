7475ac189a6e53bb8fc21bcb6e9ce4a9d27a3017
Trond Hindenes <trond@hindenes.com>
2016-11-19 00:24:47 +0100

    many things

--- Build/artifacts.ps1
+++ Build/artifacts.ps1
@@ -2,4 +2,14 @@ foreach ($artifactName in $artifacts.keys) {
   $artifact = $artifacts[$artifactName]
   $artifactName = $artifact.name 
   Write-output "pushing artifact metadata for $artifactName"
+  $obj = "" | select commit_id, package_name, commit_author, commit_tag, package_version, artifact_url, branch_name, commit_message
+  $obj.commit_id = $env:APPVEYOR_REPO_COMMIT
+  $obj.package_name = $artifactName
+  $obj.commit_author = $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
+  $obj.commit_tag = $env:APPVEYOR_REPO_TAG
+  $obj.package_version = $env:APPVEYOR_BUILD_VERSION
+  $obj.artifact_url = $artifact.url
+  $obj.branch_name = $env:APPVEYOR_REPO_BRANCH
+  $obj.commit_message = $env:APPVEYOR_REPO_COMMIT_MESSAGE
+  write-output $obj
 }
